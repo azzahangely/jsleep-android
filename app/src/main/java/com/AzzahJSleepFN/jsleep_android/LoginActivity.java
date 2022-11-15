c97675a3ae017f52fd8b795c82f189768c4d1c04
Azzah Azkiyah <azzah.azkiyah@ui.ac.id>
2022-11-15 17:54:54 +0700

    CS Modul 8

---
+++ app/src/main/java/com/AzzahJSleepFN/jsleep_android/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.AzzahJSleepFN.jsleep_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView register = findViewById(R.id.Registernow);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(move);
+            }
+        });
+    }
+
+
+
+
+}
